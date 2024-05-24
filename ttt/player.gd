2afabea13ef991d5b2b31298feb2798a0fee2ee2
B-mo <msh300msh600@gmail.com>
2024-05-24 22:29:02 +0300

    542154

--- ttt/player.gd
+++ ttt/player.gd
@@ -1,9 +1,16 @@
 extends RigidBody2D
 
+const body = preload("res://ttt/body.tscn")
+const apple = preload("res://ttt/apple.tscn")
+
 @onready var move = $move
+@onready var spown_body = $spown_body
+#@onready var pin_joint_2d = $PinJoint2D
 
 var speed = 25.0
 var angular = 5.0
+
+var x_x : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -25,4 +32,24 @@ func _move():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	_quit()
-	_move()
+	if x_x == false:
+		_move()
+
+
+func _on_hb_area_entered(area):
+	if area.get_name() == "apple":
+		var nnn = apple.instantiate()
+		nnn.global_position = Vector2(randi_range(931,-931),randi_range(-1421,1421))
+		get_parent().add_child(nnn)
+		
+		var nn = body.instantiate()
+		nn.global_position = spown_body.global_position
+		#nn.$PinJoint2D#.node_b = "player"
+		body.$p
+		get_parent().add_child(nn)
+		
+		area.queue_free()
+
+func _on_hb_body_entered(body):
+	if body.get_name() == "TileMap":
+		x_x = true
